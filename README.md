99d398388e6f016f687c205c5001c718b839deed
Dominick <dominickfabian@gmail.com>
2018-11-05 17:19:41 -0600

    update script and log meeting

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Plaeddit
 A plant support system and social experiment
+
+Meeting 11/5: 5:00pm - 5:30pm, all team members present
--- aggiebot.py
+++ aggiebot.py
@@ -34,7 +34,48 @@ def tally_votes(submission):
     water_votes = 0
     no_water_votes = 0
     for comment in submission.comments:
-        # search for the keywords
+        already_seen = False
+        if comment.author == "AggieBot":
+            continue
+
+        # check to see if I have replied to the comment already
+        for reply in comment.replies:
+            if reply.author == 'AggieBot':
+                # we've already tallied this post
+                already_seen = True
+                break
+
+        if not already_seen:
+            # search for the keywords
+            vote = parse_comment(comment.body)
+
+            # reply to the comment
+            vote_text = ""
+            if vote == 1:
+                vote_text = "water"
+            elif vote == -1:
+                vote_text = "not water"
+            elif vote == 0:
+                # remove the comment
+                comment.delete()
+                continue
+            body = "Understood! I'll tally your vote to " + vote_text + " the plant!"
+            comment.reply(body)
+
+            # count the vote
+
+
+def parse_comment(text):
+    ''' this function takes in a string representing the body of a comment and 
+    returns either a 1 symbolizing `water` or a -1 symbolizing `not water` '''
+
+    previous_word = ""
+    for word in text.split():
+        if word == "water":
+            return -1 if previous_word == "no" else 1
+        previous_word = word
 
 #make_daily_post()
 #get_todays_post()
+
+print parse_comment("this is a long comment that will eventually say water and then end")
