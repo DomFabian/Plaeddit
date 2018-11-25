028aac7db40952ecd37732a47fc434ee1d71ed31
hayleyeckert <hayleyeckert@gmail.com>
2018-11-25 14:50:51 -0600

    dot

--- pi_speak.py
+++ pi_speak.py
@@ -33,7 +33,7 @@ for row in cur.fetchall():
     mois_value = row[2]
     date = str(row[3])
 
-cmd_beg= 'espeak -g20 -ven+f2 '
+cmd_beg= 'espeak -g10 -ven+f2 '
 cmd_end= '  2>/dev/null' # To play back the stored .wav file and to dump the std errors to /dev/null
 
 # FIXME: need to get temp_value, mois_value, humid_value
@@ -42,13 +42,13 @@ print str(temp_value)
 print str(humid_value)
 print str(mois_value)
 
-text = ('Welcome_to_the_Daily_Plant_Update._Thanks_for_tuning_in._' + 'Today_is_'
+text = ('Welcome_to_the_Daily_Plant_Update_Thanks_for_tuning_in_' + 'Today_is_'
 + datetime.datetime.now().strftime("%m") + '_' + datetime.datetime.now().strftime("%d")
 + '_' + datetime.datetime.now().strftime("%Y")
-+ '._The_current_temperature_of_the_plant_is_' + str(temp_value) + '_degrees_Farenheit._'
-+ '.The_current_humidity_of_the_plant_is_' + str(humid_value) + '_percent._'
-+ '.The_current_moisture_of_the_plant_is_' + str(mois_value)
-+ '_percent._Do_not_forget_to_comment_on_todays_post_to_help_care_for_the_plant!')
++ '_The_current_temperature_of_the_plant_is_' + str(temp_value) + '_degrees_Farenheit_'
++ 'The_current_humidity_of_the_plant_is_' + str(humid_value) + '_percent_'
++ 'The_current_moisture_of_the_plant_is_' + str(mois_value)
++ '_percent_Do_not_forget_to_comment_on_todays_post_to_help_care_for_the_plant')
 
 #Calls the Espeak TTS Engine to read aloud a Text
 call([cmd_beg+text+cmd_end], shell=True)
